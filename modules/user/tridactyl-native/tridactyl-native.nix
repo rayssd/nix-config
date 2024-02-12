0f68c3d6c074f88269e852283b4320fcceed892c
Ray <rayssd@hotmail.com>
2024-02-12 17:42:17 +1100

    tridactyl-native working

--- modules/user/tridactyl-native/tridactyl-native.nix
+++ modules/user/tridactyl-native/tridactyl-native.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ... }:
 
 {
-  home.file.".mozilla/native-messenger-hosts/tridactyl.json".source = {
-    source = "${pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json";
-  };
+  home.file.".mozilla/native-messaging-hosts/tridactyl.json".text = ''
+    {
+      "name": "tridactyl",
+      "description": "Tridactyl native command handler",
+      "path": "${pkgs.tridactyl-native}/bin/native_main",
+      "type": "stdio",
+      "allowed_extensions": [ "tridactyl.vim@cmcaine.co.uk","tridactyl.vim.betas@cmcaine.co.uk", "tridactyl.vim.betas.nonewtab@cmcaine.co.uk" ]
+    }
+  '';
+
+    # source = /${builtins.unsafeDiscardStringContext pkgs.tridactyl-native}/lib/mozilla/native-messaging-hosts/tridactyl.json;
 }
